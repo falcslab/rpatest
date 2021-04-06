2cf922fe7bef386053afc593112ff6e9b7138a57
Kazuaki Kiyomi <kazuaki.kiyomi@falcs.jp>
2021-04-07 08:46:36 +0900

    mod

--- js/accessdb.js
+++ js/accessdb.js
@@ -33,25 +33,6 @@ function bulkputdb() {
   });
 }
 
-
-// function putData() {
-//   db.friends
-//     // データ挿入(Promiseが返る)
-//     .put({
-//       name: "佐藤",
-//       age: 20,
-//       hobby: "川下り",
-//     })
-//     // エラー処理
-//     .catch((error) => {
-//       console.error(error);
-//     });
-// }
-
-// async function getStartionByStNm(stNm) {
-//   return await getDbStationByStNm(stNm)
-// }
-
 async function getDbStationByStNm(stFrNm, stToNm) {
   let stFrArr = [];
   let stToArr = [];
@@ -62,11 +43,10 @@ async function getDbStationByStNm(stFrNm, stToNm) {
 
   // tmpテーブルをtruncate
   await db.tmp.clear()
-  // .then(() => {
+
   // エラーメッセージを消す
   $("div#searchError").remove()
-  // })
-  // .then(() => {
+
   await db.station
     .where("station_name")
     .equals(stFrNm.trim())
@@ -80,12 +60,10 @@ async function getDbStationByStNm(stFrNm, stToNm) {
       stFrArr.push(tmpStArr);
     })
 
-  // .then(() => {
   if (stFrArr.length == 0) {
     errormsg = errormsg + "該当する出発地が見つかりませんでした。"
   }
-  // })
-  // .then(() => {
+
   await db.station
     .where("station_name")
     .equals(stToNm.trim())
@@ -99,7 +77,7 @@ async function getDbStationByStNm(stFrNm, stToNm) {
       };
       stToArr.push(tmpToArr);
     })
-  // .then(() => {
+
   if (stToArr.length == 0) {
     if (errormsg != "") {
       // 出発地エラーの場合、頭に改行を入れる
@@ -113,10 +91,7 @@ async function getDbStationByStNm(stFrNm, stToNm) {
     $("div#errorinfo").append(errorbox.replace("{$errormsg}", errormsg))
     return
   }
-  // })
-  // .then(() => {
-  // forループが先に終わった後DB登録されている。。。
-  // dbアクセスをforループの中に入れたくない
+
   for (const stFr of stFrArr) {
     for (const stTo of stToArr) {
       // 出発駅の路線上に到着駅が存在するかチェック
@@ -124,7 +99,6 @@ async function getDbStationByStNm(stFrNm, stToNm) {
       const resLineBySt = await db.join
         .get({ "line_cd": stFr.line_cd, "station_cd1": stTo.station_cd })
 
-      // .then((res) => {
       if (resLineBySt != null) {
         result.push({ "line_cd": stFr.line_cd, "station_name_fr": stFr.station_name, "station_name_to": stTo.station_name })
       }
@@ -143,9 +117,6 @@ async function getDbStationByStNm(stFrNm, stToNm) {
       //   $("div#errorinfo").append(errorbox.replace("{$errormsg}", errormsg))
       //   return
       // })
-      // .then(() => {
-
-      // })
     }
   }
 
@@ -154,7 +125,6 @@ async function getDbStationByStNm(stFrNm, stToNm) {
       const resLine = await db.line
         .get({ "line_cd": rs.line_cd })
 
-      // .then((line) => {
       // 路線名を配列に追加
       rs.line_name = resLine.line_name
 
@@ -164,19 +134,13 @@ async function getDbStationByStNm(stFrNm, stToNm) {
 
       // 【未対応】ラジオボタンの選択によって末尾の文言変更
 
-      // })
-      // .then(() => {
       // tmpテーブルに検索結果を保存
       await db.tmp.put(rs)
         .catch((error) => {
           console.log(error);
         });
-      // })
     }
   }
-  // })
-  // })
-  // })
 }
 
 // tmpテーブルから全件取得
@@ -186,66 +150,12 @@ async function getTmpData() {
     .then((tmp) => {
       tmpArr.push(tmp)
     })
-    .then(() => {
-      // 画面表示用にパラメータ変換
-      for (let tm of tmpArr) {
-        $("span#searchDate").text(tm[0].search_date)
-        $("span#searchTime").text(tm[0].search_time)
-        $("span#route1disp").text(tm[0].station_name_fr + " → （" + tm[0].line_name + "） → " + tm[0].station_name_to);
-      }
-      console.log("getTmpDataEnd")
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-}
 
-// ===========================================================================
-// テスト用Function
-function getData() {
-  let stArr = [];
-  let lineArr = [];
-  let addrtext = "";
-  db.station
-    .where("station_name")
-    .equals($(".transitForm #sfrom").val())
-    .each((station) => {
-      let tmpStArr = {
-        line_cd: station.line_cd,
-        station_cd: station.station_cd,
-        station_name: station.station_name,
-        address: station.address,
-      };
-      stArr.push(tmpStArr);
-    })
-    .then((station) => {
-      if (stArr.length != 0) {
-        for (let st of stArr) {
-          db.line
-            .get(st.line_cd)
-            .then((line) => {
-              let tmpLineArr = {
-                line_cd: line.line_cd,
-                line_name_h: line.line_name_h,
-              };
-              lineArr.push(tmpLineArr);
-            }).then((line) => {
-              for (let line of lineArr) {
-                if (st.line_cd == line.line_cd) {
-                  addrtext =
-                    addrtext + st.line_cd + "," + st.station_cd + "," + st.station_name + "," + line.line_name_h + "," + st.address + "\n";
-                }
-              }
-              $(".transitForm #result").val(addrtext);
-            })
-            .catch((error) => {
-              console.error(error);
-            });
-        }
-      }
-    })
-    //   エラー処理
-    .catch((error) => {
-      console.error(error);
-    });
+  // 画面表示用にパラメータ変換
+  for (let tm of tmpArr) {
+    $("span#searchDate").text(tm[0].search_date)
+    $("span#searchTime").text(tm[0].search_time)
+    $("span#route1disp").text(tm[0].station_name_fr + " → （" + tm[0].line_name + "） → " + tm[0].station_name_to);
+  }
+
 }
