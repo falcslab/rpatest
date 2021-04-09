e354fd08fadc8bb6bafebf0808015899f5901508
Kazuaki Kiyomi <kazuaki.kiyomi@falcs.jp>
2021-04-09 23:33:43 +0900

    mod

--- js/index.js
+++ js/index.js
@@ -1,28 +1,29 @@
 $(document).ready(function () {
   //DB初期化
-  initdb()
-  bulkputdb()
+  initdb();
+  bulkputdb();
 
-  $(".transitForm #title").val("");
-  $(".transitForm #body").val("");
+  // $(".transitForm #title").val("");
+  // $(".transitForm #body").val("");
 
-  //イベントハンドル
-  $("#btnTest").on("click", function () {
-    getData();
+  $("#isFir").change(function () {
+    $("#hh, #mm").prop("disabled", true);
   });
-  // テストコード
-  $("#ticket").change(function () {
-    $("#expkind").prop('disabled', true);
+  $("#isDep,#isArr,#isLas,#isAvr").change(function () {
+    $("#hh, #mm").prop("disabled", false);
   });
   $("#btnSearch").on("click", function () {
     // ボタン連打対策
     $("#btnSearch").prop("disabled", true);
-    getDbStationByStNm($(".transitForm #sfrom").val(), $(".transitForm #sto").val())
+    getDbStationByStNm(
+      $(".transitForm #sfrom").val(),
+      $(".transitForm #sto").val()
+    )
       .then(() => {
-        window.location.href = './result.html';
+        window.location.href = "./result.html";
       })
       .catch((error) => {
         // 何もしない
-      })
-  })
+      });
+  });
 });
